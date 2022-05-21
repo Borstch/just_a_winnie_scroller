1bd5899db5220fba05a6dd707c2233ae77bcd274
Borstch <boris_malgin@mail.ru>
2022-05-22 00:37:25 +0500

    fix: fixed moving bees animation; adjusted scrolling speed

--- entities/moving_bee.py
+++ entities/moving_bee.py
@@ -1,5 +1,7 @@
 from typing import Union
 
+import pygame
+
 from .bee import Bee
 
 
@@ -13,6 +15,9 @@ class MovingBee(Bee):
         super(MovingBee, self).__init__(x, y, scrolling_speed)
         self._direction = direction
 
+        if self._should_flip_sprites():
+            self._sprites = [pygame.transform.flip(sprite, flip_x=True, flip_y=False) for sprite in self._sprites]
+
     def update(self, scrolling_speed: float) -> None:
         super(MovingBee, self).update(scrolling_speed)
         self._x += self._culc_movement_direction()
@@ -24,3 +29,6 @@ class MovingBee(Bee):
             return self._scrolling_speed
         else:
             return 0.0
+
+    def _should_flip_sprites(self) -> bool:
+        return self._direction == "RIGHT"
